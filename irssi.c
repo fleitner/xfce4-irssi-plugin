0c9a48ec1b582dccc450716ebd992aca943a41a2
Flavio Leitner <fbl@redhat.com>
2018-01-20 00:47:30 -0200

    Bootstrapping the XFCE4 plugin
    Signed-off-by: Flavio Leitner <fbl@redhat.com>

---
+++ irssi.c
@@ -0,0 +1,47 @@
+/* Copyright (c) 2018 Flavio Leitner <fbl@sysclose.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <gtk/gtk.h>
+#include <libxfce4panel/libxfce4panel.h>
+
+struct irssi_plugin {
+	XfcePanelPlugin *plugin;
+};
+
+
+static struct irssi_plugin *
+irssi_create(XfcePanelPlugin *plugin)
+{
+	struct irssi_plugin *irssi;
+
+	irssi = g_new(struct irssi_plugin, 1);
+	irssi->plugin = plugin;
+
+	return irssi;
+}
+
+static void
+irssi_construct (XfcePanelPlugin *plugin)
+{
+	struct irssi_plugin *irssi;
+
+	irssi = irssi_create(plugin);
+
+	g_free(irssi);
+}
+
+XFCE_PANEL_PLUGIN_REGISTER (irssi_construct);
